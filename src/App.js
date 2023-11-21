1e99d122bcb4dc8f7b7fee90014af5e4427a27a5
sangwaniKondowe <josephgmkonda@gmail.com>
2023-11-21 09:32:50 +0400

    first comit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Header from './components/nav/Header'
+import About from './components/about/About'
+import Footer from './components/footer/Footer'
+import Contact from './components/contact/Contact'
+import Testimonial from './components/testimonial/Testimonial'
+import Services from './components/services/Services'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Header/>
+    <About/>
+    <Contact/>
+    <Services/>
+    <Testimonial/>
+    <Footer/>
+    
+    </>
   )
 }
 
---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import './testimonial.css'
+
+function Testimonial() {
+  return (
+    <div>testimonial</div>
+  )
+}
+
+export default Testimonial

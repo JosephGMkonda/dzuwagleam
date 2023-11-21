29fc44f737e6769225fad1c3a17418aae4b45b9d
sangwaniKondowe <josephgmkonda@gmail.com>
2023-11-21 20:04:22 +0400

    ---------

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-
 import Header from './components/nav/Header'
 import About from './components/about/About'
 import Footer from './components/footer/Footer'
--- src/components/nav/Header.jsx
+++ src/components/nav/Header.jsx
@@ -4,10 +4,21 @@ import './header.css'
 
 function Header() {
   return (
-    <div>
+    <div className='header'>
+
+      <a href="#" className='logo'>Dzuwagleam</a>
+
+
+     <nav className='navbar'>
+     <a href="#" >Home</a>
+     <a href="#" >About</a>
+     <a href="#" >Services</a>
+     <a href="#" >Testmonials</a>
+     <a href="#">Contact Us</a>
+
+     </nav>
 
       
-
     </div>
   )
 }
